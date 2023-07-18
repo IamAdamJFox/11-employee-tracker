b8f36191e39251136d9fa1164bf1f0482b2eb151
adamjfox <adamjeffersonfox@gmail.com>
2023-07-17 20:17:50 -0700

    servers should be good

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
+// Necessary imports
 const express = require('express');
-const htmlRouter = require('./routes/html.js');
+const htmlRoutes = require('./Routes/html');
+const apiRoutes = require('./Routes/api');
 
+// Set the port for the application
 const PORT = process.env.PORT || 3001;
+
+// Create an Express application
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(express.static('public'));
-app.use('/', htmlRouter);
 
+// Routes
+app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
