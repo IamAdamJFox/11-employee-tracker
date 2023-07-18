7b45cf5ddf3608148c377cf20e66f7183fe8ad33
adamjfox <adamjeffersonfox@gmail.com>
2023-07-17 21:35:02 -0700

    yes yes yes

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 // Necessary imports
 const express = require('express');
-const htmlRoutes = require('./Routes/html.js');
-const apiRoutes = require('./Routes/api.js');
+// const htmlRoutes = require('./Routes/html.js');
+// const apiRoutes = require('./Routes/api.js');
 
 // Set the port for the application
 const PORT = process.env.PORT || 3001;
 
 // Create an Express application
 const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+res.sendFile(path.join(__dirname, '/public/assets/index.html'))
+);
 
 
 // Start the server
