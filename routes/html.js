a370030e6983909529292dfbcb9941d9cf799611
adamjfox <adamjeffersonfox@gmail.com>
2023-07-18 12:38:14 -0700

    done

--- routes/html.js
+++ routes/html.js
@@ -1,16 +1,15 @@
+//imports
 const express = require('express');
 const path = require('path');
 
-
-
 const routes = express.Router();
-
+//homepage route
 routes.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
 });
-
+//notes page route
 routes.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/notes.html'));
+  res.sendFile(path.resolve(__dirname, '..', 'public', 'notes.html'));
 });
-
+//exports
 module.exports = routes;
