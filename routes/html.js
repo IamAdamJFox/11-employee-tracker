32c9a4f923435218cd585646e2cf5a52377a39a0
adamjfox <adamjeffersonfox@gmail.com>
2023-07-17 20:47:15 -0700

    added more to json

--- routes/html.js
+++ routes/html.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('path');
+
 
 const route = express.Router();
 const routes = express.Router();
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Necessary imports
 const express = require('express');
-const htmlRoutes = require('./Routes/html');
-const apiRoutes = require('./Routes/api');
+const htmlRoutes = require('./Routes/html.js');
+const apiRoutes = require('./Routes/api.js');
 
 // Set the port for the application
 const PORT = process.env.PORT || 3001;
@@ -9,13 +9,6 @@ const PORT = process.env.PORT || 3001;
 // Create an Express application
 const app = express();
 
-// Middleware
-app.use(express.json());
-app.use(express.static('public'));
-
-// Routes
-app.use('/', htmlRoutes);
-app.use('/api', apiRoutes);
 
 // Start the server
 app.listen(PORT, () => {
